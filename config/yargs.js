c6b0e1609b101930e3b7961706398c6e7e3298a3
esteban <esteban.icc@gmail.com>
2019-12-02 19:29:03 -0500

    commit 7

--- config/yargs.js
+++ config/yargs.js
@@ -1,27 +1,27 @@
-const descripcion = {
+const descripcion = {//objeto para caracteristicas de comando descripcion
     demand: true,
     alias: 'd',
     desc: "Descripción de la tarea por hacer"
 };
 
-const completado = {
+const completado = {//objeto para caracteristicas de comando de estado
     default: true,
     alias: 'c',
     desc: "Marca como completada o pendiente la tarea"
 };
 
-const argv = require('yargs')
-    .command('crear', 'Crear una tarea', {
+const argv = require('yargs')//llamado a el paquete de yargs
+    .command('crear', 'Crear una tarea', {//comado crear tarea
         descripcion
     })
-    .command('actualizar', 'Actualiza una tarea', {
+    .command('actualizar', 'Actualiza una tarea', {//comado actualizar tarea
         descripcion,
         completado
     })
-    .command('borrar', 'Elimina una tarea', {
+    .command('borrar', 'Elimina una tarea', {//comado borrar tarea
         descripcion
     })
-    .command('listar', 'lista una tarea', {
+    .command('listar', 'lista una tarea', {//comado listar tarea
         completado
     })
     .help()
